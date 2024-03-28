bca1538decc028bdf3d7a269228cc1c98b5cd1e4
Chekiro <chekiro@wp.pl>
2024-03-28 16:53:20 +0100

    Blog Page

--- resources/views/layouts/shared/header-right-guest.blade.php
+++ resources/views/layouts/shared/header-right-guest.blade.php
@@ -1,10 +1,8 @@
 <div class="flex space-x-5">
-    <a class="flex space-x-2 items-center hover:text-yellow-500 text-sm text-gray-500"
-        href="http://127.0.0.1:8000/login">
-        Login
-    </a>
-    <a class="flex space-x-2 items-center hover:text-yellow-500 text-sm text-gray-500"
-        href="http://127.0.0.1:8000/register">
-        Register
-    </a>
+    <x-responsive-nav-link href="{{ route('login') }}" :active="request()->routeIs('login')">
+        {{ __('Login') }}
+    </x-responsive-nav-link>
+    <x-responsive-nav-link href="{{ route('register') }}" :active="request()->routeIs('register')">
+        {{ __('Register') }}
+    </x-responsive-nav-link>
 </div>
